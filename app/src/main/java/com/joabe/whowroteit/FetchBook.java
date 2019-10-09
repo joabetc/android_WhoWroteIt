b271277fbbeaaed5d2177cdd1e5c5254b9b93b28
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2019-10-09 10:02:03 -0300

    Creating an empty AsyncTask class

---
+++ app/src/main/java/com/joabe/whowroteit/FetchBook.java
@@ -0,0 +1,27 @@
+package com.joabe.whowroteit;
+
+import android.os.AsyncTask;
+import android.widget.TextView;
+
+import java.lang.ref.WeakReference;
+
+public class FetchBook extends AsyncTask<String, Void, String> {
+
+    private WeakReference<TextView> mTitleText;
+    private WeakReference<TextView> mAuthorText;
+
+    public FetchBook(TextView titleText, TextView authorText) {
+        this.mTitleText = new WeakReference<>(titleText);
+        this.mAuthorText = new WeakReference<>(authorText);
+    }
+
+    @Override
+    protected String doInBackground(String... strings) {
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+    }
+}
