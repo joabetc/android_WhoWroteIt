dc9adeb88c2471d1d44cd1c4331256a02cd9fe50
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2019-10-09 09:53:51 -0300

    Getting user input

--- app/src/main/java/com/joabe/whowroteit/MainActivity.java
+++ app/src/main/java/com/joabe/whowroteit/MainActivity.java
@@ -3,16 +3,28 @@ package com.joabe.whowroteit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText mBookInput;
+    private TextView mTitleText;
+    private TextView mAuthorText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBookInput = findViewById(R.id.bookInput);
+        mTitleText = findViewById(R.id.titleText);
+        mAuthorText = findViewById(R.id.authorText);
     }
 
     public void searchBooks(View view) {
+        String queryString = mBookInput.getText().toString();
     }
 }
